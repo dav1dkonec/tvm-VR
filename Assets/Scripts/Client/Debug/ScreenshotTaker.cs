befbbaf992be913f5b5e4236e2e66038dab9eb3f
dav1dkonec <davkon04@seznam.cz>
2026-05-04 17:10:25 +0200

    keybord screenshot taker

--- Assets/Scripts/Client/Debug/ScreenshotTaker.cs
+++ Assets/Scripts/Client/Debug/ScreenshotTaker.cs
@@ -1,18 +1,40 @@
 using System;
 using System.IO;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace TvmVr2.Client.DebugTools
 {
     [DisallowMultipleComponent]
     public sealed class ScreenshotTaker : MonoBehaviour
     {
-        [SerializeField] private KeyCode captureKey = KeyCode.Alpha0;
+        [SerializeField] private Key captureKey = Key.F9;
         [SerializeField] private int superSize = 1;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void EnsureInstance()
+        {
+            if (FindFirstObjectByType<ScreenshotTaker>() != null)
+                return;
+
+            var screenshotTaker = new GameObject("Screenshot Taker");
+            DontDestroyOnLoad(screenshotTaker);
+            screenshotTaker.AddComponent<ScreenshotTaker>();
+        }
+
+        private void Start()
+        {
+            UnityEngine.Debug.Log($"ScreenshotTaker: press {captureKey} to save a screenshot.");
+        }
 
         private void Update()
         {
-            if (Input.GetKeyDown(captureKey))
+            var keyboard = Keyboard.current;
+            if (keyboard == null || captureKey == Key.None)
+                return;
+
+            var key = keyboard[captureKey];
+            if (key != null && key.wasPressedThisFrame)
                 CaptureScreenshot();
         }
 
@@ -26,7 +48,7 @@ namespace TvmVr2.Client.DebugTools
             var path = Path.Combine(directory, fileName);
             ScreenCapture.CaptureScreenshot(path, Mathf.Max(1, superSize));
 
-            Debug.Log($"ScreenshotTaker: saved screenshot to {path}");
+            UnityEngine.Debug.Log($"ScreenshotTaker: saved screenshot to {path}");
         }
 
         private static string GetProjectRootPath()
